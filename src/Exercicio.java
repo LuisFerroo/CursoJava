46ba346c038641654553e54be2e63eef9a46754b
Luis Ferro <luisferro321@gmail.com>
2024-04-26 10:03:18 -0300

    corrigindo pula linha escalavel

--- src/Exercicio.java
+++ src/Exercicio.java
@@ -11,8 +11,10 @@ public class Exercicio{
         double measure = 53.234567;
         System.out.println("Products:");
         System.out.printf("%s wich price is $ %.2f%n",product1,price1);
-        System.out.printf("%s wich price is $ %.2f%n%n",product2,price2);
-        System.out.printf("%d years old, code %d and gender: %c%n%n",age,code,gender);
+        System.out.printf("%s wich price is $ %.2f%n",product2,price2);
+        System.out.println();
+        System.out.printf("%d years old, code %d and gender: %c%n",age,code,gender);
+        System.out.println();
         System.out.printf("Measure with eight decimal places: %.8f%n",measure);
         System.out.printf("Rouded (three decimal places): %.3f%n",measure);
         Locale.setDefault(Locale.US);
