136eee879c221c50a32c389d4f10abae588fbeb0
Luis Ferro <luisferro321@gmail.com>
2024-06-01 15:18:02 -0300

    IntervaloValor

---
+++ src/EstruturaCondicional/Intervalor.java
@@ -0,0 +1,28 @@
+package EstruturaCondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Intervalor {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        double valor = sc.nextDouble();
+        sc.close();
+        if (valor < 0 || valor > 100) {
+            System.out.println("Fora de intervalo");
+        }
+        else if (valor <= 25) {
+            System.out.println("Intervalo [0,25]");
+        }
+        else if (valor <= 50) {
+            System.out.println("Intervalo (25,50]");
+        }
+        else if (valor <= 75) {
+            System.out.println("Intervalo (50,75]");
+        }
+        else {
+            System.out.println("Intervalo (75,100]");
+        }
+    }
+}
