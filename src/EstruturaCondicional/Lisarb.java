bb87b8a4dbc7088cddd3ab0d42ccb75f1fb58351
Luis Ferro <luisferro321@gmail.com>
2024-06-02 11:33:24 -0300

    atribuicao acumulativa

--- src/EstruturaCondicional/Lisarb.java
+++ src/EstruturaCondicional/Lisarb.java
@@ -8,19 +8,19 @@ public class Lisarb {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         double renda = sc.nextDouble();
-        double imposto;
+        double imposto = 80.00;
         sc.close();
         if (renda <= 2000.00) {
             imposto = 0.00;
         }
         else if (renda <= 3000.00) {
-            imposto = (renda-2000.00)*0.08;
+            imposto = (renda-2000) * 0.08;
         }
         else if (renda <= 4500.00) {
-            imposto = (1000.00*0.08)+((renda-3000)*0.18);
+            imposto += ((renda-3000)*0.18);
         }
         else {
-            imposto = (1000.00*0.08)+(1500*0.18)+((renda-4500)*0.28);
+            imposto += (1500*0.18)+((renda-4500)*0.28);
         }
         if (imposto == 0) {
             System.out.println("Isento");
